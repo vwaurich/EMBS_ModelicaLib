7d389a4859cf1366b4ef57299483b542f12ceb72
vwaurich <volker.waurich@gmx.de>
2020-05-28 20:16:51 +0200

    done the main

--- ReadSID/ReadSID/src/main.cpp
+++ ReadSID/ReadSID/src/main.cpp
@@ -9,9 +9,32 @@
 static int lineNumber;
 
 
-void printOrigin(origin o) {
-	std::cout << "origin (order:" << o.order <<")"<< std::endl;
-	std::cout << "ncol:" << o.ncol << " nrow:" << o.nrow << " nqn:" << o.nq << " n_nqn" << o.nqn << std::endl;
+void printM_2(double* M, int dimRow, int dimCol) {
+	for (int r = 0; r < dimRow; r++) {
+		for (int c = 0; c < dimCol; c++) {
+			std::cout << M[c + r * dimCol] << " \t ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void printM_3(double* M, int dimRow, int dimNQ, int dimCol) {
+	for (int r = 0; r < dimRow; r++) {
+		for (int c = 0; c < dimCol; c++) {
+			for (int nq = 0; nq < dimNQ; nq++) {
+				std::cout << "M[" << r+1 << ", " << nq+1 << ", " << c+1 << "] = " << M[c+r*dimCol+nq*dimCol*dimRow] << std::endl;
+			}
+		}
+	}
+}
+
+void printTaylor(taylor t) {
+	std::cout << "\t" << "order:" << t.order << ")" << std::endl;
+	std::cout << "\t" << "ncol:" << t.ncol << " nrow:" << t.nrow << " nqn:" << t.nq << " n_nqn" << t.nqn << std::endl;
+	std::cout << "\t" << "______________M0__________________" << std::endl;
+	printM_2(t.M0, t.nrow, t.ncol);
+	std::cout << "\t" << "______________M1__________________" << std::endl;
+	printM_3(t.M1, t.nrow, t.nq, t.ncol);
 }
 
 
@@ -46,12 +69,27 @@ int allocateSIDstructure(std::string line, SID_Data* sid)
 
 double stringDouble_SID(std::string s) {
 	double base = 0.0;
-	double decExponent = 0.0f;
+	double decExponent = 0.0;
+	double ret = 0.0;
 	int pos = 0;
-	pos = s.find("D+", pos);
-	base = std::stod(s.substr(0, pos));
-	decExponent = std::stod(s.substr(pos + 2, s.length()));
-	return base * pow(10, decExponent);
+	pos = s.find("D+", 0);
+	if(pos != std::string::npos)
+	{
+		base = std::stod(s.substr(0, pos));
+		decExponent = std::stod(s.substr(pos + 2, s.length()));
+		ret = base * pow(10, decExponent);
+	}
+	else {
+		pos = s.find("D-", 0);
+		if (pos != std::string::npos)
+		{
+			base = std::stod(s.substr(0, pos));
+			decExponent = std::stod(s.substr(pos + 2, s.length()));
+			ret = base * pow(10, -decExponent);
+		}
+	}
+
+	return ret;
 }
 
 std::string stringBetween(std::string s, std::string start, std::string stop) {
@@ -137,11 +175,11 @@ void setMatrix2D(double value, double* mat, int r, int c, int numR, int numC)
 
 void setMatrix3D(double value, double* mat, int r, int nq, int c, int numR, int numNQ, int numC)
 {
-	//traverse the rows, than the qn dim than the cols
-	mat[(r - 1) * numC * numNQ + (nq-1)*numC + c-1] = value;
+	//traverse the cols,rows and q
+	mat[(nq - 1) * numC * numR + (r-1)*numC + c-1] = value;
 }
 
-int parseOrigin(std::ifstream* sidFile, SID_Data* sid) {
+int parseTaylor(std::ifstream* sidFile, taylor* tayl) {
 	std::string line;
 	std::stringstream ss(line);
 	std::string str;
@@ -150,36 +188,36 @@ int parseOrigin(std::ifstream* sidFile, SID_Data* sid) {
 	{
 		if (findString(line, "order"))
 		{
-			sid->nodes[sid->currNodeIdx].orig.order = std::stoi(stringBetween(line, "=", "/n"));
+			tayl->order = std::stoi(stringBetween(line, "=", "/n"));
 		}
 		else if (findString(line, "nrow"))
 		{
-			sid->nodes[sid->currNodeIdx].orig.nrow = std::stoi(stringBetween(line, "=", "/n"));
+			tayl->nrow = std::stoi(stringBetween(line, "=", "/n"));
 		}
 		else if (findString(line, "ncol"))
 		{
-			sid->nodes[sid->currNodeIdx].orig.ncol = std::stoi(stringBetween(line, "=", "/n"));
+			tayl->ncol = std::stoi(stringBetween(line, "=", "/n"));
 		}
 		else if (findString(line, "nq "))//Attention, the space seperates the token from nqn
 		{
-			sid->nodes[sid->currNodeIdx].orig.nq = std::stoi(stringBetween(line, "=", "/n"));
+			tayl->nq = std::stoi(stringBetween(line, "=", "/n"));
 		}
 		else if (findString(line, "nqn "))
 		{
-			sid->nodes[sid->currNodeIdx].orig.nqn = std::stoi(stringBetween(line, "=", "/n"));
+			tayl->nqn = std::stoi(stringBetween(line, "=", "/n"));
 		}
 		else if (findString(line, "structure"))
 		{
-			sid->nodes[sid->currNodeIdx].orig.structure = std::stoi(stringBetween(line, "=", "/n"));
+			tayl->structure = std::stoi(stringBetween(line, "=", "/n"));
 			//after structure, lets allocate the matrices
-			sid->nodes[sid->currNodeIdx].orig.M0 = (double*)calloc(sid->nodes[sid->currNodeIdx].orig.ncol *
-				sid->nodes[sid->currNodeIdx].orig.nrow, sizeof(double));
-			sid->nodes[sid->currNodeIdx].orig.M1 = (double*)calloc(sid->nodes[sid->currNodeIdx].orig.ncol *
-				sid->nodes[sid->currNodeIdx].orig.nrow *
-				sid->nodes[sid->currNodeIdx].orig.nq, sizeof(double));
-			sid->nodes[sid->currNodeIdx].orig.Mn = (double*)calloc(sid->nodes[sid->currNodeIdx].orig.ncol *
-				sid->nodes[sid->currNodeIdx].orig.nrow *
-				sid->nodes[sid->currNodeIdx].orig.nqn, sizeof(double));
+			tayl->M0 = (double*)calloc(tayl->ncol *
+				tayl->nrow, sizeof(double));
+			tayl->M1 = (double*)calloc(tayl->ncol *
+				tayl->nrow *
+				tayl->nq, sizeof(double));
+			tayl->Mn = (double*)calloc(tayl->ncol *
+				tayl->nrow *
+				tayl->nqn, sizeof(double));
 		}
 		else if (findString(line, "m0"))
 		{
@@ -187,7 +225,7 @@ int parseOrigin(std::ifstream* sidFile, SID_Data* sid) {
 			int idxc=0;
 			getIndeces2(line, &idxr, &idxc);
 			double val = stringDouble_SID(stringBetween(line, "=", "/n"));
-			setMatrix2D(val, sid->nodes[sid->currNodeIdx].orig.M0, idxr, idxc, sid->nodes[sid->currNodeIdx].orig.nrow, sid->nodes[sid->currNodeIdx].orig.ncol);
+			setMatrix2D(val, tayl->M0, idxr, idxc, tayl->nrow, tayl->ncol);
 		}
 		else if (findString(line, "m1"))
 		{
@@ -196,7 +234,7 @@ int parseOrigin(std::ifstream* sidFile, SID_Data* sid) {
 			int idxc = 0;
 			getIndeces3(line, &idxr, &idxnq, &idxc);
 			double val = stringDouble_SID(stringBetween(line, "=", "/n"));
-			setMatrix3D(val, sid->nodes[sid->currNodeIdx].orig.M1, idxr, idxnq, idxc, sid->nodes[sid->currNodeIdx].orig.nrow, sid->nodes[sid->currNodeIdx].orig.nq, sid->nodes[sid->currNodeIdx].orig.ncol);
+			setMatrix3D(val, tayl->M1, idxr, idxnq, idxc, tayl->nrow, tayl->nq, tayl->ncol);
 		}
 		else if (findString(line, "mn"))
 		{
@@ -204,9 +242,9 @@ int parseOrigin(std::ifstream* sidFile, SID_Data* sid) {
 			int idxnqn = std::stoi(stringBetween(line, ",", ","));
 			int idxc = std::stoi(stringBetween(line, ",", ")"));
 			double val = stringDouble_SID(stringBetween(line, "=", "/n"));
-			setMatrix3D(val, sid->nodes[sid->currNodeIdx].orig.Mn, idxr, idxnqn, idxc, sid->nodes[sid->currNodeIdx].orig.nrow, sid->nodes[sid->currNodeIdx].orig.nqn, sid->nodes[sid->currNodeIdx].orig.ncol);
+			setMatrix3D(val, tayl->Mn, idxr, idxnqn, idxc, tayl->nrow, tayl->nqn, tayl->ncol);
 		}
-		else if (findString(line, "end origin")) {
+		else if (findString(line, "end")) {
 			return 0;
 		}
 	}
@@ -227,16 +265,19 @@ int parseNode(std::ifstream* sidFile, SID_Data* sid) {
 		}
 		else if (findString(line, "origin"))
 		{
-			parseOrigin(sidFile, sid);
+			parseTaylor(sidFile, &sid->nodes[sid->currNodeIdx].orig);
 		}
 		else if (findString(line, "phi"))
 		{
+			parseTaylor(sidFile, &sid->nodes[sid->currNodeIdx].phi);
 		}
 		else if (findString(line, "psi"))
 		{
+			parseTaylor(sidFile, &sid->nodes[sid->currNodeIdx].psi);
 		}
 		else if (findString(line, "AP"))
 		{
+			parseTaylor(sidFile, &sid->nodes[sid->currNodeIdx].AP);
 		}	
 		else if (findString(line, "end node")) {
 			return 0;
@@ -279,6 +320,50 @@ int parseModal(std::ifstream* sidFile, SID_Data* sid) {
 		else if (findString(line, "frame"))
 		{
 			parseFrame(sidFile, sid);
+		}
+		else if (findString(line, "mdCM"))
+		{
+			parseTaylor(sidFile, &sid->mdCM);
+		}
+		else if (findString(line, "J"))
+		{
+			parseTaylor(sidFile, &sid->J);
+		}
+		else if (findString(line, "Ct"))
+		{
+			parseTaylor(sidFile, &sid->Ct);
+		}
+		else if (findString(line, "Cr"))
+		{
+			parseTaylor(sidFile, &sid->Cr);
+		}
+		else if (findString(line, "Me"))
+		{
+			parseTaylor(sidFile, &sid->Me);
+		}
+		else if (findString(line, "Gr"))
+		{
+			parseTaylor(sidFile, &sid->Gr);
+		}
+		else if (findString(line, "Ge"))
+		{
+			parseTaylor(sidFile, &sid->Ge);
+		}
+		else if (findString(line, "Oe"))
+		{
+			parseTaylor(sidFile, &sid->Oe);
+		}
+		else if (findString(line, "ksigma"))
+		{
+			parseTaylor(sidFile, &sid->ksigma);
+		}
+		else if (findString(line, "Ke"))
+		{
+			parseTaylor(sidFile, &sid->Ke);
+		}
+		else if (findString(line, "De"))
+		{
+			parseTaylor(sidFile, &sid->De);
 		}
 		else if (findString(line,"end modal")) {
 			return 0;
